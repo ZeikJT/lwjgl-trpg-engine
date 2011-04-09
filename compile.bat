9db282630b7c055426bf36c8826618b062cf2d77
ZeikJT <zeikjt@gmail.com>
2011-04-09 07:39:23 +0000

    Fixing up compile paths.

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-javac -cp jar\lwjgl.jar;jar\jinput.jar;jar\slick-util.jar TRPG.java
-java -cp .;assets;jar\lwjgl.jar;jar\jinput.jar;jar\slick-util.jar -Djava.library.path=native\windows TRPG
+javac -cp jar/lwjgl.jar;jar/jinput.jar;jar/slick-util.jar TRPG.java
+java -cp .;assets;jar/lwjgl.jar;jar/jinput.jar;jar/slick-util.jar -Djava.library.path=native/windows TRPG
