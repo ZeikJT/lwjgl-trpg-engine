13b926b50ab5c4127ef7f1a112f49ed2c141231a
MatiasGiachino@gmail.com <MatiasGiachino@gmail.com@e024ba6f-d26c-45fb-9d4a-7913346f8509>
2011-04-09 07:39:23 +0000

    Fixing up compile paths.

--- trunk/compile.sh
+++ trunk/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac -cp jar\lwjgl.jar:jar\jinput.jar:jar\slick-util.jar TRPG.java
-java -cp .:assets:jar\lwjgl.jar:jar\jinput.jar:jar\slick-util.jar -Djava.library.path=native\linux TRPG
+javac -cp jar/lwjgl.jar:jar/jinput.jar:jar/slick-util.jar TRPG.java
+java -cp .:assets:jar/lwjgl.jar:jar/jinput.jar:jar/slick-util.jar -Djava.library.path=native/linux TRPG
