5dfe0070490a04feddc6ac7499fc0af33063d1f5
Matias Giachino <matiasgiachino@gmail.com>
2015-03-22 00:56:10 -0700

    mkdir for compiled directory when scripts run

--- compile.bat
+++ compile.bat
@@ -1,2 +1,3 @@
-javac -d compiled -cp jar/lwjgl.jar;jar/jinput.jar;jar/slick-util.jar;src src/TRPG/TRPG.java
-java -cp .;jar/lwjgl.jar;jar/jinput.jar;jar/slick-util.jar;assets;compiled -Djava.library.path=native/windows TRPG.TRPG
+if not exist compiled mkdir compiled
+javac -d compiled -cp jar/lwjgl.jar;jar/jinput.jar;jar/slick-util.jar;src src/TRPG/Main.java
+java -cp .;jar/lwjgl.jar;jar/jinput.jar;jar/slick-util.jar;assets;compiled -Djava.library.path=native/windows TRPG.Main
